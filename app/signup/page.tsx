0210a26644148f410e6b8b01f127b4e3a9ad40a8
Emmanuel <emmauelsoniran005@gmail.com>
2026-03-19 23:09:08 +0100

    remove google option

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -346,11 +346,11 @@ export default function SignUpPage() {
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-border"></div>
               </div>
-              <div className="relative flex justify-center text-xs uppercase">
+              {/* <div className="relative flex justify-center text-xs uppercase">
                 <span className="bg-background px-2 text-muted-foreground">
                   Or continue with
                 </span>
-              </div>
+              </div> */}
             </div>
 
             {/* <Button
